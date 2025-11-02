83c3d79b9f4ab91a7d214376524321d86078dff0
wangym <wangym0203@gmail.com>
2025-11-02 23:03:18 +0800

    # feat: improve send email title

--- src/infrastructure/notifier.py
+++ src/infrastructure/notifier.py
@@ -65,7 +65,7 @@ class EmailNotifier(INotifier):
             target_date = datetime.strptime(date_str, "%Y-%m-%d").date()
             today = datetime.now().date()
             delta = (target_date - today).days
-            
+
             weekday_names_cn = ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
 
             if delta < 0:
